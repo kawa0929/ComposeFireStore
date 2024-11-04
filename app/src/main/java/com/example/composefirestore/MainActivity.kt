0e1a5d6fe55fff3bcec3fca44e29fd20f1112a64
student <melisaqq55@gmail.com>
2024-11-04 16:59:06 +0800

    v12_刪除資料

--- app/src/main/java/com/example/composefirestore/MainActivity.kt
+++ app/src/main/java/com/example/composefirestore/MainActivity.kt
@@ -143,7 +143,11 @@ fun Birth(m: Modifier){
             }) {
                 Text("查詢資料")
             }
-            Button(onClick = {  }) {
+            Button(onClick = { db.collection("users")
+                .document(userName)
+                .delete()
+                msg = "刪除資料"
+            }) {
                 Text("刪除資料")
             }
         }
